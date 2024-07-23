eed0cab31d96147ef3f803b36414d05ca85c6584
Leandro <63678585+Liuth16@users.noreply.github.com>
2024-07-23 09:43:06 -0300

    Add ex 03 and 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayObject, ...remove) {
+    const trueArray = Array.from(arrayObject);
+    for (const element of remove){
+        let index = trueArray.indexOf(element);
+        while (index > -1){
+          trueArray.splice(index,1);
+          index = trueArray.indexOf(element);
+        }
+    }
+    return trueArray;
 };
 
 // Do not edit below this line
