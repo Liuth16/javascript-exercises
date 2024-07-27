14c77984f27d7ce8d3d903c8c759ca1d0df030c2
Leandro <63678585+Liuth16@users.noreply.github.com>
2024-07-27 10:00:33 -0300

    Add solution to exercises 05 and 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(initial, final) {
+    let total = 0;
+    if(typeof initial !== 'number'|| typeof final !== 'number'){
+      return "ERROR";
+    }
 
+    else if (initial < 0 || final <0){
+      return "ERROR";
+    }
+    else {
+      if (initial > final){
+        temp = initial
+        initial = final;
+        final = temp;
+        console.log(initial, final)
+      }
+
+      for (initial; initial <= final; initial++){
+          total += initial;
+      }
+      return total;
+  }
 };
 
 // Do not edit below this line
