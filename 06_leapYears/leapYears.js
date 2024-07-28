35ffa06dad6d327135797d211025cf57cf9dab52
Leandro <63678585+Liuth16@users.noreply.github.com>
2024-07-28 10:25:16 -0300

    Add solution to ex 07 and 08, remove unnecessary variable from ex 06.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,5 @@
 const leapYears = function(year) {
-    const years = year;
-    if (years%4 === 0 && (years%100 !== 0 || years%400 === 0)){
+    if (year%4 === 0 && (year%100 !== 0 || year%400 === 0)){
         return true;
     } else{
         return false;
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let total = 0;
+  for (let i = 0; i < numbers.length; i++){
+    total += numbers[i];
+    console.log(total);
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let total = 1;
+  for (let i = 0; i < numbers.length; i++){
+    total = total * numbers[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(number, expoent) {
+	return number ** expoent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let fact = 1;
+	for (let i = 1; i <= number; i++){
+    fact = fact*i;
+  }
+  return fact;
+
 };
 
 // Do not edit below this line
