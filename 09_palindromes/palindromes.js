9e9472aa6f52d2795b4327164d48cc32d792c697
Leandro <63678585+Liuth16@users.noreply.github.com>
2024-07-28 18:09:32 -0300

    Add solution to exercises 09 and 10.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const text = [...string];
+    stringText = text.filter(char => {return !/[^a-zA-Z0-9]/.test(char);}).join('').toLowerCase();
+    reversed = text.reverse();
+    stringReversed = text.filter(char => {return !/[^a-zA-Z0-9]/.test(char);}).join('').toLowerCase();
+    if (stringReversed === stringText){
+      return true;
+    } else {
+      return false;
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    position = Number(position);
+    let firstNum = 1;
+    let secondNum = 0;
+    let currentNum = 0;
+    if (position < 0){
+      return "OOPS";
+    } else if (position === 1){
+      return 1;
+    } else{
+      for (let i = 2; i <= position; i++){
+      currentNum = firstNum + secondNum;
+      secondNum =firstNum;
+      firstNum = currentNum;
+    }
+    return currentNum;
+    }
 };
 
 // Do not edit below this line
